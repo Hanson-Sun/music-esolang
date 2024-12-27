6f429219e7e414410927a3ada9fd441e13a9a72f
Hanson Sun <65196939+Hanson-Sun@users.noreply.github.com>
2024-12-27 11:33:37 -0800

    simple debugging

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@
 #include <string>
 #include <vector>
 
-void printHelp(const char* programName) {
-    std::cout << "Usage: " << programName << " [options] [midi_file.mid]\n";
-    std::cout << "Options:\n";
-    std::cout << "  -h, --help\t\tDisplay this help message\n";
-    std::cout << "  [midi_file.mid]\tPath to a MIDI file to interpret\n";
+
+void printHelp(const std::string& programName) {
+    std::cout << "Usage: " << programName << " [options] [filename]" << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "  -h, --help                   Print this help message" << std::endl;
+    std::cout << "  -p, --parse    [filename]    Print the parsed AST" << std::endl;
+    std::cout << "  -t, --tokenize [filename]    Print the tokenized results" << std::endl;
     std::cout << "\nIf no MIDI file is provided, the interpreter will start in interactive REPL mode.\n";
 }
 
@@ -86,6 +88,38 @@ void run(const std::string midiFilename) {
     }
 }
 
+void printParsedResults(const std::string& filename) {
+    Midi midi = MidiReader::read(filename);
+    Tokenizer tokenizer(*midi.begin());
+
+    ASTPrinter printer;
+
+    Parser parser(tokenizer);
+    Parser::ParserIterator it = parser.begin();
+    while(it) {
+        auto result = *it;
+        if (_check(result)) {
+            ErrorHandler::printError(std::get<Error>(result));
+            break;
+        }
+
+        std::get<Statement_t>(result)->accept(printer);
+        ++it;
+    }
+    std::cout << std::endl;
+}
+
+void printTokenizedResults(const std::string& filename) {
+    Midi midi = MidiReader::read(filename);
+    Tokenizer tokenizer(*midi.begin());
+    Tokenizer::TokenizerIterator tokenizerIt = tokenizer.begin();
+    while (tokenizerIt) {
+        std::cout << (*tokenizerIt).toString() << " ";
+        ++tokenizerIt;
+    }
+    std::cout << std::endl;
+}
+
 int main(int argc, char* argv[]) {
 
     for (int i = 1; i < argc; ++i) {
@@ -96,15 +130,19 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (argc >= 2) {
-        std::string midiFilename = argv[1];
-        run(midiFilename);
-    } else if (argc == 1) {
+    if (argc == 1) {
         repl();
+    } else if (std::string arg = std::string(argv[1]); (arg == "-p" || arg == "--parse") && argc == 3) {
+        std::string midiFilename = argv[2];
+        printParsedResults(midiFilename);
+    } else if (std::string arg = std::string(argv[1]); (arg == "-t" || arg == "--tokenize") && argc == 3) {
+        std::string midiFilename = argv[2];
+        printTokenizedResults(midiFilename);
     } else {
         std::cerr << "Incorrect parameters. " << std::endl;
         printHelp(argv[0]);
         return 1;
     }
+
     return 0;
 }
